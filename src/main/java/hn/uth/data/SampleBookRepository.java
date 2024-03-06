d78f8747967d936b58892d8ee30548e24ddb581c
torhu05 <vhsandoval@uth.hn>
2024-03-06 00:01:18 -0600

    actualizacion
    creacion repositorio

---
+++ src/main/java/hn/uth/data/SampleBookRepository.java
@@ -0,0 +1,9 @@
+package hn.uth.data;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SampleBookRepository extends JpaRepository<SampleBook, Long>, JpaSpecificationExecutor<SampleBook> {
+
+}
